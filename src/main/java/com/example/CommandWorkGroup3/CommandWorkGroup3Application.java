6de90bcd645b448172f04ee105f264023ac9cf06
Алексей Шлапаков <theblack2swan@gmail.com>
2024-10-03 15:54:10 +0300

    CommandWorkGroup3

---
+++ src/main/java/com/example/CommandWorkGroup3/CommandWorkGroup3Application.java
@@ -0,0 +1,13 @@
+package com.example.CommandWorkGroup3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommandWorkGroup3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommandWorkGroup3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CommandWorkGroup3/CommandWorkGroup3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CommandWorkGroup3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommandWorkGroup3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
